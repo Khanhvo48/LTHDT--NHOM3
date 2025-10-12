7bf4df7e3220740f7496b0540bfd56b097043269
Hungp <hungphamduy456654@gmail.com>
2025-10-12 23:14:07 +0700

    Thuc hanh tuan 6: Tinh ke thua va da hinh trong Java
    - Tao lop SachGiaoTrinh ke thua tu Sach voi thuoc tinh monHoc va capDo
    - Tao lop SachTieuThuyet ke thua tu Sach voi thuoc tinh theLoai va laSachSeries
    - Tao lop QuanLySach de quan ly danh sach sach voi cac chuc nang CRUD
    - Tao lop Test voi ham main de demo cac chuc nang
    - Cap nhat lop Sach voi phuong thuc toString()
    - Demo tinh da hinh thong qua override toString() va hienThiThongTin()

---
+++ TUAN5/Sach.java
@@ -0,0 +1,81 @@
+// Lớp Sach thể hiện thông tin của một cuốn sách
+public class Sach {
+
+    private String maSach;
+    private String tieuDe;
+    private String tacGia;
+    private int namXuatBan;
+    private int soLuong;
+
+    // Constructor không tham số
+    public Sach() {
+    }
+
+    // Constructor có tham số (dùng để khởi tạo nhanh đối tượng)
+    public Sach(String maSach, String tieuDe, String tacGia, int namXuatBan, int soLuong) {
+        this.maSach = maSach;
+        this.tieuDe = tieuDe;
+        this.tacGia = tacGia;
+        this.namXuatBan = namXuatBan;
+        this.soLuong = soLuong;
+    }
+
+    // Giúp đảm bảo tính đóng gói (Encapsulation)
+    public String getMaSach() {
+        return maSach;
+    }
+
+    public void setMaSach(String maSach) {
+        this.maSach = maSach;
+    }
+
+    public String getTieuDe() {
+        return tieuDe;
+    }
+
+    public void setTieuDe(String tieuDe) {
+        this.tieuDe = tieuDe;
+    }
+
+    public String getTacGia() {
+        return tacGia;
+    }
+
+    public void setTacGia(String tacGia) {
+        this.tacGia = tacGia;
+    }
+
+    public int getNamXuatBan() {
+        return namXuatBan;
+    }
+
+    public void setNamXuatBan(int namXuatBan) {
+        this.namXuatBan = namXuatBan;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public void hienThiThongTin() {
+        System.out.println("Mã sách: " + maSach);
+        System.out.println("Tiêu đề: " + tieuDe);
+        System.out.println("Tác giả: " + tacGia);
+        System.out.println("Năm xuất bản: " + namXuatBan);
+        System.out.println("Số lượng: " + soLuong);
+    }
+    
+    // Phương thức toString() để hiển thị thông tin cơ bản
+    @Override
+    public String toString() {
+        return "Mã sách: " + maSach + 
+               "\nTiêu đề: " + tieuDe + 
+               "\nTác giả: " + tacGia + 
+               "\nNăm xuất bản: " + namXuatBan + 
+               "\nSố lượng: " + soLuong;
+    }
+}
